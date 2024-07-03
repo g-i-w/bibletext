3379b8f4cf080ad78f84bf5d810821f66663c3be
Gabriel Wilson <giw@wilsons.life>
2024-07-03 16:12:35 -0500

    integrated BibleSearch into Bible.java, so now there is basically just Bible.java and Strongs.java, with various classes to insert data into each.  'And, behold, I come quickly; and my reward is with me, to give every man according as his work shall be.' Rev 22:12

--- Bible.java
+++ Bible.java
@@ -16,8 +16,14 @@ public class Bible {
 	Map<String,String> bookAliases;
 	
 	Tree bibleText;
+	Tree bibleData;
+
 	Tree wordLookup;
 	Tree wordData;
+	
+	Tree bibleCompressed;
+	
+	Tree wordCompounds;
 	
 	private void increment ( Tree leaf ) {
 		String valStr = leaf.value();
@@ -34,8 +40,10 @@ public class Bible {
 	public Bible ( String wordRegex ) {
 		this.wordRegex = wordRegex;
 		bibleText = new JSON( JSON.RETAIN_ORDER );
+		bibleData = new JSON( JSON.RETAIN_ORDER );
 		wordLookup = new JSON( JSON.RETAIN_ORDER );
 		wordData = new JSON( JSON.RETAIN_ORDER );
+		bibleCompressed = new JSON( JSON.RETAIN_ORDER );
 		bookAliases = new HashMap<>();
 	}
 	
@@ -43,27 +51,51 @@ public class Bible {
 	
 	public void importChapter ( String book, String chap, List<String> verses ) throws Exception {
 		int size = verses.size();
+		bibleData.auto( book ).auto( "chapters" ).add( chap );
 		for (int i=0; i<size; i++) {
 			importVerse( book, chap, String.valueOf(i+1), verses.get(i).trim() ); // verses start at 1 instead of 0
 		}
 	}
 	
 	public void importVerse ( String book, String chap, String verse, String text ) throws Exception {
+		// add the verse text
 		Tree chapObj = bibleText.auto( book ).auto( chap ).add( verse, text );
+		
+		// loop through each word in the verse
 		for (String word : Regex.groups( text, wordRegex )) {
+			// filter down to simple characters ("basic" form)
 			String basic = alphabet.filter( word );
 			if (basic.length()>0) {
-				wordLookup.auto( basic ).auto( book ).auto( chap ).add( verse, chapObj.get( verse ) );
-				if (!wordData.keys().contains(basic)) wordData.auto( basic ).auto( "id" ).add( String.valueOf(wordCount++) );
+				// add to word lookup
+				wordLookup.auto( basic ).auto( book ).auto( chap ).add( verse, chapObj.get(verse) );
+				
+				// auto-increment the word ID
+				if (!wordData.keys().contains(basic)) wordData.auto( basic ).add( "id", String.valueOf(wordCount++) );
+				
+				// add the byte hex-string for the word
 				//wordData.auto( basic ).add( "bytes", base16( basic ) );
+				
+				// add the byte hex-string LIST for the word
 				if (!wordData.auto( basic ).keys().contains("chars")) wordData.auto( basic ).auto( "chars" ).add( base16List( basic ) );
+				
+				// also add the "full" word
 				increment( wordData.auto( basic ).auto( "full" ).auto( word ) );
+				
+				// add the verse ID to the compressed structure
+				String wordId = wordData.get(basic).get("id").value();
+				bibleCompressed.auto( "text" ).auto( book ).auto( chap ).auto( verse ).add( wordId );
+				bibleCompressed.auto( "words" ).add( wordId, basic );
+				bibleCompressed.auto( "lookup" ).auto( basic ).auto( book ).auto( chap ).add( verse );
 			}
 		}
 	}
 	
 	public Set<String> books () {
 		return bibleText.keys();
+	}
+	
+	public Tree data () {
+		return bibleData;
 	}
 	
 	public Map<String,String> aliases () {
@@ -77,10 +109,12 @@ public class Bible {
 	
 	public void alias ( String alias, String book ) {
 		bookAliases.put( alias, book );
+		bibleData.auto( book ).add( "name", alias );
+		bibleData.auto( book ).add( "bytes", new Bytes(alias.getBytes( StandardCharsets.UTF_16BE )).toString() );
 	}
 
 	public String verse ( String refStr ) throws Exception {
-		List<String> ref = Regex.groups( refStr, "(\\S+)\\s*(\\d+):(\\d+)" );
+		List<String> ref = Regex.groups( refStr, "([^\\d\\s]+)\\s*(\\d+):(\\d+)" );
 		String book = ref.get(0);
 		String chap = ref.get(1);
 		String verse = ref.get(2);
@@ -88,6 +122,7 @@ public class Bible {
 	}
 	
 	public String verse ( String book, String chap, String verse ) throws Exception {
+		System.out.println( book+" "+chap+":"+verse );
 		return bibleText.get( alias(book) ).get( chap ).get( verse ).value();
 	}
 	
@@ -100,8 +135,7 @@ public class Bible {
 	}
 	
 	public Tree wordLookup ( String word ) {
-		if (wordLookup.keys().contains( word )) return wordLookup.get( word );
-		else return null;
+		return wordLookup.get( word );
 	}
 	
 	public Tree wordData () {
@@ -130,5 +164,40 @@ public class Bible {
 		if (wordData.keys().contains( word )) return wordData.get(word).get("full");
 		else return null;
 	}
+
+	public void buildSearch () {
+		wordCompounds = new JSON( JSON.AUTO_ORDER );
+		for (String word : wordLookup().keys()) {
+			int wordLength = word.length();
+			wordCompounds.auto( word ).auto( "lookup" ).add( word, wordLookup.get( word ) );
+			wordCompounds.auto( word ).auto( "data" ).auto( "this" ).add( word, wordData.get( word ) );
+			for (int size=2; size<wordLength; size++) {
+				int maxPos = wordLength-size;
+				for (int pos=0; pos<=maxPos; pos++) {
+					String subWord = word.substring(pos, pos+size);
+					//System.out.println( subWord );
+					if (wordLookup().keys().contains( subWord )) {
+						//System.out.println( word+" "+subWord );
+						wordCompounds.auto( word ).auto( "data" ).auto( "sub" ).add( subWord, String.valueOf( wordLookup.get(subWord).keys().size() ) );
+						wordCompounds.auto( subWord ).auto( "data" ).auto( "super" ).add( word, String.valueOf( wordLookup.get(word).keys().size() ) );
+					}
+				}
+			}
+		}
+		
+	}
 	
+	public Tree compounds () {
+		return wordCompounds;
+	}
+	
+	public Tree search ( String word ) {
+		word = alphabet.filter(word);
+		if (wordCompounds.keys().contains(word)) {
+			return wordCompounds.get(word);
+		} else {
+			return null;
+		}
+	}
+
 }
--- EBibleOrgText.java
+++ EBibleOrgText.java
@@ -1,43 +1,44 @@
 package bibletext;
 
-import java.nio.charset.StandardCharsets;
 import java.util.*;
 import java.io.*;
+import java.nio.file.*;
 import creek.*;
 import paddle.*;
 
 public class EBibleOrgText {
 
 	public static void text ( Bible bible, String path ) throws Exception {
+		Alphabet alpha = new Alphabet();
 		for (File file : FileActions.recurse(path)) {
 			List<String> ref = Regex.groups( file.getName(), "(\\w{3})_(\\d{2})_read.txt$" );
 			if (ref.size()==2) {
 				String book = ref.get(0);
 				String chap = Regex.first( ref.get(1), "([^0]\\d*)" ); // trim leading zeros
-				List<String> lines = FileActions.readLines( file );
-				String fullBookName = Regex.first( lines.get(0), "(\\S+).$" );
-				fullBookName = fullBookName.replace("\uFEFF", ""); // see https://stackoverflow.com/questions/54247407/why-utf-8-bom-bytes-efbbbf-can-be-replaced-by-ufeff, previously I found EF BB BF at the beginning, which is UTF8-speak for FE FF (UTF-16)
+				List<String> lines = FileActions.readLines( file, FileActions.UTF8 );
+				String fullBookName = alpha.filter( lines.get(0) );
+				//System.out.println( book+","+fullBookName+","+(new Bytes(fullBookName.getBytes( FileActions.UTF16BE )).toString()) );
+				// no longer needed because of filter: //fullBookName = fullBookName.replace("\uFEFF", ""); // see https://stackoverflow.com/questions/54247407/why-utf-8-bom-bytes-efbbbf-can-be-replaced-by-ufeff, previously I found EF BB BF at the beginning, which is UTF8-speak for FE FF (UTF-16)
 				bible.alias( fullBookName, book );
 				lines.remove(0);
 				lines.remove(0); // remove first two lines
 				bible.importChapter( book, chap, lines );
 			}
 		}
+		bible.buildSearch();
 	}
 	
 	public static void main ( String[] args ) throws Exception {
 		Bible bible = new Bible();
 		EBibleOrgText.text( bible, args[0] );
 		
-		BibleSearch search = new BibleSearch( bible, 2 );
-		
-		System.out.println( bible.wordData().serialize() );
+		//System.out.println( bible.wordData().serialize() );
+		System.out.println( bible.data().serialize() );
 		System.out.println( bible.aliases() );
 		
-		for (int i=1; i<args.length; i++) {
-			System.out.println( "\n"+args[i]+":\n"+search.search( args[i] ).serialize() ); // BibleSearch.search
-			//System.out.println( "\n"+args[i]+":\n"+bible.verse( args[i] ) ); // Bible.verse
-		}
+		if (args.length>1) System.out.println( "\n"+args[1]+":\n"+bible.verse( args[1] ) );              // verse( )
+		if (args.length>2) System.out.println( "\n"+args[2]+":\n"+bible.search( args[2] ).serialize() ); // search( )
+		
 	}
 
 }
