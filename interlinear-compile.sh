485b4feecf94e4292b3e81f1f366fe1abcbf4fd7
Gabriel Wilson <giw@wilsons.life>
2024-12-20 16:58:18 -0600

    compiled into a working BibleLookup.html! Praise the Lord!

---
+++ interlinear-compile.sh
@@ -0,0 +1,7 @@
+#! /usr/bin/sh
+
+cat interlinear-start.html >> interlinear-merged.html
+cat aliases.js >> interlinear-merged.html
+cat interlinear.js >> interlinear-merged.html
+cat interlinear-end.html >> interlinear-merged.html
+
