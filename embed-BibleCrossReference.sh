2f22f6a9cee4f27c53716e2e2ae0dd35e0312b7d
Gabriel Wilson <giw@wilsons.life>
2025-01-13 16:37:00 -0600

    added embed-BibleCrossReference shell script and main html files for lang-specific versions of BibleCrossReference in matt2446.us.

---
+++ embed-BibleCrossReference.sh
@@ -0,0 +1,9 @@
+java creek.HTML bibletext/BibleCrossReferenceECHAB.html matt2446.us/BibleCrossReference.html
+java creek.HTML bibletext/BibleCrossReferenceChinese.html matt2446.us/BibleCrossReference-Chinese.html
+java creek.HTML bibletext/BibleCrossReferenceHindi.html matt2446.us/BibleCrossReference-Hindi.html
+java creek.HTML bibletext/BibleCrossReferenceSpanish.html matt2446.us/BibleCrossReference-Spanish.html
+java creek.HTML bibletext/BibleCrossReferenceFrench.html matt2446.us/BibleCrossReference-French.html
+java creek.HTML bibletext/BibleCrossReferenceArabic.html matt2446.us/BibleCrossReference-Arabic.html
+java creek.HTML bibletext/BibleCrossReferenceBengali.html matt2446.us/BibleCrossReference-Bengali.html
+java creek.HTML bibletext/BibleCrossReferenceRussian.html matt2446.us/BibleCrossReference-Russian.html
+
